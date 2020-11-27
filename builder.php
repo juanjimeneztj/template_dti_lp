b3ab26d43e7682b5e4ab26201c9ff86e78952daa
Juan Jimenez <juanjimeneztj@gmail.com>
2020-11-27 08:31:45 -0800

    added builder structure

---
+++ builder.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+        <!--Stylesheets-->
+        <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+        <!--Title-->
+        <title>Builder &#124; FinMC/SIP</title>
+    </head><!--End head-->
+    <body>
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-9 col-lg-8 col-xl-9">
+                        content
+                    </div>
+                    <div class="col">
+                        Elements
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <script src="./node_modules/jquery/jquery.min.js"></script>
+        <script src="./node_modules/popper.js/dist/umd/popper.min.js"></script>
+        <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body><!--End body-->
+</html><!--End html-->
