63dec63fec83a99b33c44201c1bc589ee5a48db3
Juan Jimenez <juanjimeneztj@gmail.com>
2020-11-27 08:46:19 -0800

    added section to the elements and testing function to get all elements

--- builder.php
+++ builder.php
@@ -1,32 +1,46 @@
 <!DOCTYPE html>
 <html>
-    <head>
-       <meta charset='utf-8'>
-        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-        <meta name='viewport' content='width=device-width, initial-scale=1'>
 
-        <!--Stylesheets-->
-        <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
 
-        <!--Title-->
-        <title>Builder &#124; FinMC/SIP</title>
-    </head><!--End head-->
-    <body>
-        <main>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-                        content
-                    </div>
-                    <div class="col">
-                        Elements
+    <!--Stylesheets-->
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+    <!--Title-->
+    <title>Builder &#124; FinMC/SIP</title>
+</head>
+<!--End head-->
+
+<body>
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-9 col-lg-8 col-xl-9">
+                    <div id="elements-content-gsweb">
+                        <!-- The Elements Content -->
+                        test
                     </div>
                 </div>
+                <div class="col">
+                    <h3><strong>Elements</strong></h3>
+                    <p>Here you can find all the elements to build your pages.</p>
+                </div>
             </div>
-        </main>
+        </div>
+    </main>
 
-        <script src="./node_modules/jquery/jquery.min.js"></script>
-        <script src="./node_modules/popper.js/dist/umd/popper.min.js"></script>
-        <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    </body><!--End body-->
-</html><!--End html-->
+    <script src="./node_modules/jquery/jquery.min.js"></script>
+    <script src="./node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script>
+        var $content = document.getElementById('elements-content-gsweb');
+        console.log($content.textContent);
+    </script>
+</body>
+<!--End body-->
+
+</html>
+<!--End html-->
